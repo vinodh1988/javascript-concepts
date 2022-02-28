7b3e9d798b2a172f04f8784d2f858995beac1378
vinodh1988 <k.c.vinodh@hotmail.com>
2022-02-28 14:39:40 +0530

    block-commit

--- variables.js
+++ variables.js
@@ -1,7 +1,8 @@
+/*
 //javascript does not follow type checking
 
 //a=90
-b="King"
+//b="King"
 c=true
 d=32.43
 
@@ -9,7 +10,11 @@ d=32.43
 
 //console.log(typeof(a),typeof(b),typeof(c),typeof(d))
 a=300
+//const b=89
 function fun(){
+    const b=90
+    console.log(b)
+    
     console.log(a)
     var a=500
     console.log(a)
@@ -18,3 +23,16 @@ function fun(){
 console.log(a)
 fun()
 console.log(a)
+*/
+
+function temp(){
+    var a=200;
+      console.log(a)
+    {
+           let a=100;
+           console.log("inner ",a)
+    }
+      console.log(a)
+}
+
+temp()
